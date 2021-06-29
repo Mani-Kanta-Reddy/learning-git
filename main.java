d1fb23f150570a4806970e993c526cd2f4d1b9db
Mani-Kanta-Reddy <70527498+Mani-Kanta-Reddy@users.noreply.github.com>
2021-06-29 09:01:39 +0530

    added display method
    display method just prints Hello GitHub again

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
 class Main {
+	private static void display() {
+		System.out.println("Hello GitHub Again");
+	}
 	public static void main(Stirng[] args) {
 		System.out.println("Hello gitHub");
+		display();
 	}
 }
 
